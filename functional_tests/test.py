d1bd1ecdfec8b5e0b518917ef646759276650ed2
de-elebit <de.elebit@gmail.com>
2018-07-22 21:29:40 +0900

    목록 출력을 위한 신규 URL, 뷰, 템플릿

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -18,9 +18,9 @@ class NewVisitorTest(LiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
-        self.assertIn('To-Do', self.browser.title)
+        self.assertIn('To-Do lists', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        self.assertIn('작업 목록 시작', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), '작업 아이템 입력')
